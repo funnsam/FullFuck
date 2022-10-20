91a3df25e0da874cabbe333618e33a6b646e1e31
funnsam <funnsam8@gmail.com>
2022-10-20 18:05:05 +0800

    Life of Change

--- src/main.go
+++ src/main.go
@@ -29,7 +29,6 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
 import (
-	"encoding/hex"
 	"fmt"
 	"os"
 )
@@ -89,9 +88,8 @@ func Parse(element byte) {
 		if element == '%' {
 			ParsingSpecial = 9
 			return
-		} else {
-			ParsingSpecialBuffer = append(ParsingSpecialBuffer, element)
 		}
+		ParsingSpecialBuffer = append(ParsingSpecialBuffer, element)
 	}
 	switch element {
 	case '+':
@@ -99,59 +97,18 @@ func Parse(element byte) {
 	case '-':
 		TokenList = append(TokenList, Token{LoopLayer, 1, LoopID, ""})
 	case '>':
-		if ParsingSpecial == 3 {
-			t, Uerr = hex.DecodeString(string(ParsingSpecialBuffer))
-			checkUErr()
-
-			TokenList = append(TokenList, Token{LoopLayer, 2 | uint32(t[0])<<8, LoopID, ""})
-
-			ParsingSpecial = 0
-			ParsingSpecialBuffer = make([]uint8, 0, 2)
-		} else if ParsingSpecial == 9 {
-			TokenList = append(TokenList, Token{LoopLayer, 0xFFFFFF02, LoopID, string(ParsingSpecialBuffer)})
-			ParsingSpecialBuffer = make([]uint8, 0, 2)
-			ParsingSpecial = 0
-		} else {
-			TokenList = append(TokenList, Token{LoopLayer, 258, LoopID, ""})
-		}
+		OutputToken()
 	case '<':
-		if ParsingSpecial == 3 {
-			t, Uerr = hex.DecodeString(string(ParsingSpecialBuffer))
-			checkUErr()
-
-			TokenList = append(TokenList, Token{LoopLayer, 3 | uint32(t[0])<<8, LoopID, ""})
-
-			ParsingSpecial = 0
-			ParsingSpecialBuffer = make([]uint8, 0, 2)
-		} else if ParsingSpecial == 9 {
-			TokenList = append(TokenList, Token{LoopLayer, 0xFFFFFF03, LoopID, string(ParsingSpecialBuffer)})
-			ParsingSpecialBuffer = make([]uint8, 0, 2)
-			ParsingSpecial = 0
-		} else {
-			TokenList = append(TokenList, Token{LoopLayer, 259, LoopID, ""})
-		}
+		InputToken()
+	case '[':
+		OpenBracket()
+	case ']':
+		TokenList = append(TokenList, Token{LoopLayer, 5, LoopID, ""})
+		LoopLayer--
 	case '0':
 		ParsingSpecial = 1
 	case '%':
 		ParsingSpecial = 8
-	case '[':
-		LoopID++
-		LoopLayer++
-
-		TokenList = append(TokenList, Token{LoopLayer, 4, LoopID, ""})
-		t, Uerr = hex.DecodeString(string(ParsingSpecialBuffer))
-		checkUErr()
-
-		if ParsingSpecial != 3 {
-			LoopLoopsTimes = append(LoopLoopsTimes, -1)
-		} else {
-			LoopLoopsTimes = append(LoopLoopsTimes, int16(t[0]))
-		}
-		ParsingSpecial = 0
-		ParsingSpecialBuffer = make([]uint8, 0, 2)
-	case ']':
-		TokenList = append(TokenList, Token{LoopLayer, 5, LoopID, ""})
-		LoopLayer--
 	}
 }
 
---
+++ src/tokens.go
@@ -0,0 +1,56 @@
+package main
+
+import "encoding/hex"
+
+func OutputToken() {
+	if ParsingSpecial == 3 {
+		t, Uerr = hex.DecodeString(string(ParsingSpecialBuffer))
+		checkUErr()
+
+		TokenList = append(TokenList, Token{LoopLayer, 2 | uint32(t[0])<<8, LoopID, ""})
+
+		ParsingSpecial = 0
+		ParsingSpecialBuffer = make([]uint8, 0, 2)
+	} else if ParsingSpecial == 9 {
+		TokenList = append(TokenList, Token{LoopLayer, 0xFFFFFF02, LoopID, string(ParsingSpecialBuffer)})
+		ParsingSpecialBuffer = make([]uint8, 0, 2)
+		ParsingSpecial = 0
+	} else {
+		TokenList = append(TokenList, Token{LoopLayer, 258, LoopID, ""})
+	}
+}
+
+func InputToken() {
+	if ParsingSpecial == 3 {
+		t, Uerr = hex.DecodeString(string(ParsingSpecialBuffer))
+		checkUErr()
+
+		TokenList = append(TokenList, Token{LoopLayer, 3 | uint32(t[0])<<8, LoopID, ""})
+
+		ParsingSpecial = 0
+		ParsingSpecialBuffer = make([]uint8, 0, 2)
+	} else if ParsingSpecial == 9 {
+		TokenList = append(TokenList, Token{LoopLayer, 0xFFFFFF03, LoopID, string(ParsingSpecialBuffer)})
+		ParsingSpecialBuffer = make([]uint8, 0, 2)
+		ParsingSpecial = 0
+	} else {
+		TokenList = append(TokenList, Token{LoopLayer, 259, LoopID, ""})
+	}
+}
+
+func OpenBracket() {
+	LoopID++
+	LoopLayer++
+
+	TokenList = append(TokenList, Token{LoopLayer, 4, LoopID, ""})
+	t, Uerr = hex.DecodeString(string(ParsingSpecialBuffer))
+	checkUErr()
+
+	if ParsingSpecial != 3 {
+		LoopLoopsTimes = append(LoopLoopsTimes, -1)
+	} else {
+		LoopLoopsTimes = append(LoopLoopsTimes, int16(t[0]))
+	}
+	ParsingSpecial = 0
+	ParsingSpecialBuffer = make([]uint8, 0, 2)
+}
